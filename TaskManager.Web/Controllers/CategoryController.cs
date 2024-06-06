e86d1d9384c1ae407cdd0508cb391f1b52968106
jason <jason>
2024-06-06 15:08:35 +0300

    Category Controler and beggining of Ui

---
+++ TaskManager.Web/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Web.Data;
+using TaskManager.Web.Models;
+
+namespace TaskManager.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db= db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ TaskManager.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Web.Models;
+
+namespace TaskManager.Web.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Work", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Hobby", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Self Improvment", DisplayOrder = 3 }
+                );
+        }
+    }
+}
