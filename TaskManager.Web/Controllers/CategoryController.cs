e2ffc8ffb123e68051f18e9a84fd71d3e838e67e
jason <jason>
2024-06-19 16:00:47 +0300

    Category crud

--- TaskManager.Web/Controllers/CategoryController.cs
+++ TaskManager.Web/Controllers/CategoryController.cs
@@ -9,12 +9,59 @@ namespace TaskManager.Web.Controllers
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
-            _db= db;
+            _db = db;
         }
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }
+
+        //Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot be the exact match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot be the exact match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- TaskManager.Web/Models/Category.cs
+++ TaskManager.Web/Models/Category.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskManager.Web.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)]
+
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [Range(1, 100, ErrorMessage ="Display Order must be between 1-100")]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
